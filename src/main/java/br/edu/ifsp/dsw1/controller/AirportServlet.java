5938d5bb87b180383a033c78cd7841127db51d6c
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-20 20:32:28 -0300

    Adicionando handler para a submissão do formulário.

--- src/main/java/br/edu/ifsp/dsw1/controller/AirportServlet.java
+++ src/main/java/br/edu/ifsp/dsw1/controller/AirportServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 import br.edu.ifsp.dsw1.model.entity.FlightDataCollection;
@@ -46,12 +48,29 @@ public class AirportServlet extends HttpServlet {
 		
 		if("login".equals(action)) {
 			view = handleLogin(request, response);
-		}else {
+		}else if("auth".equals(action)){
+			view = handleAuth(request,response);
+		}else{
 			view = "index.jsp";
 		}
 		
 		var dispatcher = request.getRequestDispatcher(view);
 		dispatcher.forward(request, response);
+	}
+
+	private String handleAuth(HttpServletRequest request, HttpServletResponse response) {
+		String usuario = request.getParameter("textUser");
+		String senha = request.getParameter("textSenha");
+		if(autenticar(usuario, senha)) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", usuario);
+			session.setMaxInactiveInterval(5 * 60);
+		}
+		return "flights.jsp";
+	}
+
+	private boolean autenticar(String usuario, String senha) {
+		return ("admin".equals(usuario) && "admin".equals(senha));
 	}
 
 	private String handleLogin(HttpServletRequest request, HttpServletResponse response) {
